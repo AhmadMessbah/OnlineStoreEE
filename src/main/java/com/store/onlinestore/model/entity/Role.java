e785e5df204afe75cb3aa18702c4cd4655c43310
niloo <ahmadiniloufar78@gmail.com>
2024-06-19 22:59:08 +0330

    rolecommit

---
+++ src/main/java/com/store/onlinestore/model/entity/Role.java
@@ -0,0 +1,27 @@
+package com.store.onlinestore.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@ToString
+@Entity(name = "roleEntity")
+@NamedQueries({
+        @NamedQuery(name = "Role.FindById", query = "select r from roleEntity r where r.id = id")
+})
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "role")
+    private String role;
+}
