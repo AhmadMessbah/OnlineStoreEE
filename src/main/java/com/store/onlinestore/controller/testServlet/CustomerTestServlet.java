0180cbe114c21f563f0e136e864fa52e6f1c3eee
Farnoosh <farnoshgh20@gmail.com>
2024-06-21 14:00:55 +0330

    CustomerTest updated

---
+++ src/main/java/com/store/onlinestore/controller/testServlet/CustomerTestServlet.java
@@ -0,0 +1,44 @@
+package com.store.onlinestore.controller.testServlet;
+
+import com.store.onlinestore.controller.validation.BeanValidator;
+import com.store.onlinestore.model.entity.Customer;
+import com.store.onlinestore.model.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/customer.test")
+public class CustomerTestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Customer customer =
+                    Customer
+                            .builder()
+                            .name("aaaa")
+                            .family("bbbb")
+                            .password("123456789")
+                            .email("aaa@gmail.com")
+
+                            .build();
+
+            BeanValidator<Customer> personValidator = new BeanValidator<>();
+            if(personValidator.validate(customer).isEmpty()) {
+                System.out.println(personValidator.validate(customer));
+            }else{
+                System.out.println(personValidator.validate(customer));
+            }
+
+
+            System.out.println(CustomerService.getService().findAll());
+
+            System.out.println(CustomerService.getService().findByUsername("a"));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,31 @@
+import com.store.onlinestore.controller.validation.BeanValidator;
+import com.store.onlinestore.model.entity.Customer;
+import com.store.onlinestore.model.entity.Person;
+import com.store.onlinestore.model.service.CustomerService;
+import com.store.onlinestore.model.service.PersonService;
+
+
 public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer customer =
+                Customer
+                        .builder()
+                        .name("aaaa")
+                        .family("bbbb")
+                        .password("123456789")
+                        .email("aaa@gmail.com")
+
+                        .build();
+
+        BeanValidator<Customer> customerValidator = new BeanValidator<>();
+        System.out.println(customerValidator.validate(customer));
+//
+//        System.out.println(CustomerService.getService().save(customer);
+//
+//        System.out.println(CustomerService.getService().findAll());
+//
+//        System.out.println(CustomerService.getService().findByUsernameAndPassword("a","11111"));
+    }
+
 }
