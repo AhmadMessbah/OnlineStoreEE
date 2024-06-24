d1877ae6962c904d05bab0aef352726b471527f5
arbabzadehmh <arbabzadeh.mh@gmail.com>
2024-06-24 16:50:54 +0330

    ProductGroup, ProductGroupApi, ProductGroupService, ProductGroupTestServlet updated.

--- src/main/java/com/store/onlinestore/model/entity/ProductGroup.java
+++ src/main/java/com/store/onlinestore/model/entity/ProductGroup.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -21,16 +22,19 @@ import java.util.List;
 @Table(name="product_group_tbl")
 @NamedQueries({
         @NamedQuery(name = "ProductGroup.FindByName", query = "select p from productGroupEntity  p where p.name like :name "),
-        @NamedQuery(name = "ProductGroup.FindByStatus", query = "select p from productGroupEntity p where p.status =:status")
+        @NamedQuery(name = "ProductGroup.FindByStatus", query = "select p from productGroupEntity p where p.status =:status"),
+        @NamedQuery(name = "ProductGroup.FindByParentName", query = "select p from productGroupEntity p where p.parentGroup.name =:name")
+
 })
 public class ProductGroup {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "productGroupSeq", sequenceName = "product_group_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "productGroupSeq")
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name", length = 30, unique = true)
-    @Pattern(regexp = "^[a-zA-Z\\s]{3,30}$" ,message = "Invalid Name")
+//    @Pattern(regexp = "^[a-zA-Z\\s]{3,30}$" ,message = "Invalid Name")
     private String name;
 
     @Column(name = "description", length = 30)
@@ -39,11 +43,23 @@ public class ProductGroup {
     @Column(name = "is_active")
     private boolean status;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    // TODO: ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}) روی این حالت باشد این خطا را میدهد cascade اگر
+    // TODO: Converting `org.hibernate.PersistentObjectException` to JPA `PersistenceException` : detached entity passed to persist: com.store.onlinestore.model.entity.ProductGroup
+    @ManyToOne
     @JoinTable(name = "product_group_relation_tbl")
     private ProductGroup parentGroup;
 
-    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "parentGroup")
+    // TODO: نباشد این خطا را میدهد fetch = FetchType.EAGER اگر
+    // TODO: failed to lazily initialize a collection of role: com.store.onlinestore.model.entity.ProductGroup.childGroupList: could not initialize proxy - no Session
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER, mappedBy = "parentGroup")
     private List<ProductGroup> childGroupList;
+
+
+    public void addChildGroup(ProductGroup childGroup){
+        if (childGroupList == null){
+            childGroupList = new ArrayList<>();
+        }
+        childGroupList.add(childGroup);
+    }
 
 }
