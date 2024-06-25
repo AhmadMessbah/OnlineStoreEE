7b628077c159a8378c0737dd8aa9a0fe5e04c62e
sajjadchakame <smsc7680@gmail.com>
2024-06-26 00:27:42 +0330

    Updated - sajjadchakame

--- src/main/java/com/store/onlinestore/model/entity/ProductUnit.java
+++ src/main/java/com/store/onlinestore/model/entity/ProductUnit.java
@@ -2,6 +2,7 @@ package com.store.onlinestore.model.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,21 +16,24 @@ import lombok.experimental.SuperBuilder;
 @ToString
 
 @NamedQueries({
-    @NamedQuery(name = "ProductUnit.FindByName", query = "select u from unitEntity u where u.name like :name")
+    @NamedQuery(name = "ProductUnit.FindByName", query = "select u from productUnitEntity u where u.name like :name")
 })
 
-@Entity(name = "unitEntity")
-@Table(name = "unit_tbl")
+@Entity(name = "productUnitEntity")
+@Table(name = "product_unit_tbl")
 public class ProductUnit {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "productUnitSeq", sequenceName = "product-unit-seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "productUnitSeq")
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name", length = 30)
+//    @Pattern(regexp = "^[a-zA-Z\\s]{3,30}$" ,message = "Invalid Name")
     private String name;
 
     @Column(name = "symbol")
+//    @Pattern(regexp = "^[a-zA-Z\\d]{3,30}$", message = "Invalid Symbol")
     private String symbol;
 
     @Column(name = "description")
