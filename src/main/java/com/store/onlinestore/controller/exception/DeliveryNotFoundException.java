f1acddcd1ed2455092a4179fa73492cd390a6a86
yaghoubi-mpro <mohammadself.yrafi@gmail.com>
2024-06-27 08:28:17 -0600

    entity: Address and deliver/ Address and delivery not found exception/ delivery and address service / enum : delivery status and delivery method

---
+++ src/main/java/com/store/onlinestore/controller/exception/DeliveryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.store.onlinestore.controller.exception;
+
+public class DeliveryNotFoundException extends Exception {
+    public DeliveryNotFoundException() {
+        super("Delivery Not Found !!!");
+    }
+}
+
