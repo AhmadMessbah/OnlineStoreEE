a703bf4c31a2c8ddb310a571fb9f5b7da58725f0
F-Farhanpoor <farhang.farhanpoor@gmail.com>
2024-06-27 11:34:51 +0330

    update entity 1.01

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,6 +1,8 @@
 import com.store.onlinestore.model.entity.Product;
+import com.store.onlinestore.model.service.ProductService;
 
 import java.sql.Time;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class ProductTest {
@@ -8,39 +10,37 @@ public class ProductTest {
         Product product1 =
                 Product
                         .builder()
-                        .name("sabon")
-                        .description("sabz")
+                        .name("car")
+                        .description("black")
                         .status(true)
-                        .brand("golnar")
+                        .brand("benz")
                         .unitId(null)
                         .productGroupId(null)
                         .price(10000)
-//                        .image("/img/0001.jpg")
-                        .dateOfModified(Time.valueOf(LocalTime.now()))
+                        .dateOfModified(LocalDateTime.now())
                         .barcode("021655632")
-//                        .inStock(0)
+                        .inventoryId(null)
                         .build();
 
         Product product2 =
                 Product
                         .builder()
-                        .name("mooz")
-                        .description("zard")
+                        .name("laptop")
+                        .description("xs55")
                         .status(true)
-                        .brand("amrica")
+                        .brand("asus")
                         .unitId(null)
                         .productGroupId(null)
                         .price(20000)
-//                        .image("/img/0002.jpg")
-                        .dateOfModified(Time.valueOf(LocalTime.now()))
+                        .dateOfModified(LocalDateTime.now())
                         .barcode("54684")
-//                        .inStock(1)
+                        .inventoryId(null)
                         .build();
 //        todo:dar in folder kar nemikone va error JSON Binding provider org.eclipse.yasson.JsonBindingProvider not found
-//        System.out.println(ProductService.getService().save(product2));
-//        System.out.println(ProductService.getService().save(product1));
-//        System.out.println(ProductService.getService().findAll());
-//        System.out.println(ProductService.getService().findByNameAndBrand("m","a"));
-//        System.out.println(ProductService.getService().FindByBrand("a"));
+        System.out.println(ProductService.getService().save(product2));
+        System.out.println(ProductService.getService().save(product1));
+        System.out.println(ProductService.getService().findAll());
+        System.out.println(ProductService.getService().findByNameAndBrand("c","b"));
+        System.out.println(ProductService.getService().FindByBrand("a"));
     }
 }
