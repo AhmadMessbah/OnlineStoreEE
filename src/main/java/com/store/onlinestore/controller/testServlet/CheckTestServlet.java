89bcb3026630e56011230a94f652b58398885831
Mahtab Saedi <mhtb.sd82@gmail.com>
2024-06-28 09:16:48 +0330

    Check Commit.

---
+++ src/main/java/com/store/onlinestore/controller/testServlet/CheckTestServlet.java
@@ -0,0 +1,56 @@
+package com.store.onlinestore.controller.testServlet;
+
+import com.store.onlinestore.controller.validation.BeanValidator;
+import com.store.onlinestore.model.entity.CheckPayment;
+import com.store.onlinestore.model.service.CheckService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet("/Check.test")
+public class CheckTestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            CheckPayment checkPayment1 =
+                CheckPayment
+                    .builder()
+                    .checkNumber(123456789L)
+                    .dateTime(LocalDateTime.now())
+                    .build();
+
+            CheckPayment checkPayment2 =
+                CheckPayment
+                    .builder()
+                    .checkNumber(987654321L)
+                    .dateTime(LocalDateTime.now())
+                    .build();
+
+            BeanValidator<CheckPayment> checkValidator = new BeanValidator<>();
+
+            if(checkValidator.validate(checkPayment1).isEmpty()) {
+                System.out.println(checkValidator.validate(checkPayment1));
+            }else{
+                System.out.println(checkValidator.validate(checkPayment1));
+            }
+
+            if(checkValidator.validate(checkPayment2).isEmpty()) {
+                System.out.println(checkValidator.validate(checkPayment2));
+            }else{
+                System.out.println(checkValidator.validate(checkPayment2));
+            }
+
+            System.out.println(CheckService.getService().findAll());
+
+            System.out.println(CheckService.getService().FindByCheckNumber(123456789));
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
