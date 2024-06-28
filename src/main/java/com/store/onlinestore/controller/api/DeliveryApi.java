47ea52da0e9693456babde4d4ae9dd9a2e8410e9
yaghoubi-mpro <mohammadself.yrafi@gmail.com>
2024-06-27 23:22:44 -0600

    update address delivery entity and service api and servlet and test of delivery and address

---
+++ src/main/java/com/store/onlinestore/controller/api/DeliveryApi.java
@@ -0,0 +1,18 @@
+package com.store.onlinestore.controller.api;
+
+import com.store.onlinestore.model.service.DeliveryService;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+@Path("/delivery")
+public class DeliveryApi {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll()throws Exception{
+        return Response.ok().entity(DeliveryService.getDeliveryService().deliveryFindAll()).build();
+    }
+}
