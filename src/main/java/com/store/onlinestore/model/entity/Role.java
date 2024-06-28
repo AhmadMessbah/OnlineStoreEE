18677a4367ebb870656586f06b96e2b16c89f31e
ahmadiniloufar <ahmadiniloufar78@gmail.com>
2024-06-28 09:22:14 +0330

    commiterole

---
+++ src/main/java/com/store/onlinestore/model/entity/Role.java
@@ -0,0 +1,30 @@
+package com.store.onlinestore.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@ToString
+
+@Entity(name = "roleEntity")
+@Table(name="role_tbl")
+@NamedQueries({
+        @NamedQuery(name = "Role.FindById", query = "select r from roleEntity r where r.id = :id"),
+        @NamedQuery(name = "Role.FindByRole", query = "select r from roleEntity r where r.role = :role")
+})
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+}
