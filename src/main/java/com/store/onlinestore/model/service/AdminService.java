defa73343f0e3f4119582fabd2f05abbbfc7650f
Leva_Ziaei <levaziaee3@gmail.com>
2024-06-29 17:17:43 +0330

    Admin Updated

--- src/main/java/com/store/onlinestore/model/service/AdminService.java
+++ src/main/java/com/store/onlinestore/model/service/AdminService.java
@@ -1,5 +1,4 @@
 package com.store.onlinestore.model.service;
-
 import com.store.onlinestore.controller.exception.AdminNotFoundException;
 import com.store.onlinestore.model.entity.Admin;
 import com.store.onlinestore.model.repository.CrudRepository;
@@ -28,13 +27,11 @@ public class AdminService {
         }
     }
 
-    //    todo : convert to logical remove
     public Admin remove(Long id) throws Exception {
         try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
             return repository.remove(id, Admin.class);
         }
     }
-
     public List<Admin> findAll() throws Exception {
         try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
             List<Admin> adminList = repository.findAll(Admin.class);
@@ -50,6 +47,13 @@ public class AdminService {
             throw new AdminNotFoundException();
         }
     }
+    public List<Admin> findByUsername(String username) throws Exception {
+        try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("username", username+"%");
+            return repository.executeQuery("Admin.FindByUsername", params, Admin.class);
+        }
+    }
 
     public List<Admin> findByNameAndFamily(String name, String family) throws Exception {
         try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
@@ -60,10 +64,26 @@ public class AdminService {
         }
     }
 
-    public Admin findByUsername(String username) throws Exception {
+    public List<Admin> findByUsernameAndPassword(String username, String password) throws Exception {
         try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
             Map<String, Object> params = new HashMap<>();
-            params.put("username", username);
+            params.put("username", username+"%");
+            params.put("password", password+"%");
+            return repository.executeQuery("Admin.FindByUsernameAndPassword", params, Admin.class);
+        }
+    }
+    public List<Admin> findByEmail(String email) throws Exception {
+        try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("email", email+"%");
+            return repository.executeQuery("Admin.FindByEmail", params, Admin.class);
+        }
+    }
+
+    public Admin findByPhoneNumber(String phoneNumber) throws Exception {
+        try (CrudRepository<Admin, Long> repository = new CrudRepository<>()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("phoneNumber", phoneNumber);
             List<Admin> result = repository.executeQuery("Admin.FindByPhoneNumber", params, Admin.class);
             if (result.isEmpty()) {
                 return null;
@@ -72,8 +92,8 @@ public class AdminService {
             }
         }
     }
+}
 
 //    todo : findByUsernameAndPassword, phoneNumber, email, roleName
-}
 
 
