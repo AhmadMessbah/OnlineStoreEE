aabfb04485a3d2615025f7507c431ff8c68df822
niloo <ahmadiniloufar78@gmail.com>
2024-07-11 23:11:41 +0330

    managercommite

--- src/main/java/com/store/onlinestore/model/service/ManagerService.java
+++ src/main/java/com/store/onlinestore/model/service/ManagerService.java
@@ -1,7 +1,11 @@
 package com.store.onlinestore.model.service;
 
+import com.store.onlinestore.model.entity.Customer;
 import com.store.onlinestore.model.entity.Manager;
 import com.store.onlinestore.model.repository.CrudRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
+import jakarta.persistence.PersistenceContext;
 import lombok.Getter;
 
 import java.util.HashMap;
@@ -9,97 +13,84 @@ import java.util.List;
 import java.util.Map;
 
 public class ManagerService {
-    @Getter
-    private static ManagerService service = new ManagerService();
+    @PersistenceContext(unitName = "store")
+    private EntityManager entityManager;
 
-    private ManagerService() {
-    }
 
     public Manager save(Manager manager) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            manager.setRole(RoleService.getService().FindByRole("manager"));
-            return repository.save(manager);
-        }
+        entityManager.persist(manager);
+        entityManager.find(ProductGroupService.class,1);
+   return manager;
     }
 
     public Manager edit(Manager manager) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            return repository.edit(manager);
+        Manager foundManager = entityManager.find(Manager.class, manager.getId());
+        if (foundManager != null) {
+            entityManager.merge(manager);
         }
+        return manager;
     }
 
+
     public Manager remove(Long id) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            return repository.remove(id, Manager.class);
-        }
+    Manager manager=entityManager.find(Manager.class, id);
+   if(manager!=null){
+       manager.setDeleted(true);
+       entityManager.merge(manager);
+   }
+   return manager;
     }
 
     public List<Manager> findAll() throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            return repository.findAll(Manager.class);
-        }
+        return entityManager
+                .createQuery("select n from managerEntity n where n .deleted=false", Manager.class)
+                .getResultList();
     }
-
     public Manager findById(Long id) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            return repository.findById(id, Manager.class);
-        }
+        Manager manager=entityManager.find(Manager.class,id);
+        return manager;
     }
 
     public List<Manager> findByUsername(String username) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("username", username);
-            return repository.executeQuery("Manager.FindByUsername", params, Manager.class);
-        }
+        return entityManager
+                .createQuery("select o from managerEntity o where o.username=username",Manager.class)
+                .getResultList();
     }
 
     public List<Manager> findByNameAndFamily(String name, String family) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("name", name+"%");
-            params.put("family", family+"%");
-            return repository.executeQuery("Manager.FindByNameAndFamily", params, Manager.class);
-        }
+       return entityManager
+               .createQuery("select o from managerEntity o where o.name like  :name and o.family like :family",Manager.class)
+               .setParameter("name",name+"%")
+               .setParameter("family",family+"%")
+               .getResultList();
     }
 
     public List<Manager> findByUsernameAndPassword(String username, String password) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("username", username);
-            params.put("password", password);
-            return repository.executeQuery("Manager.FindByUsernameAndPassword", params, Manager.class);
-        }
+       return entityManager
+               .createQuery("select o from managerEntity o where o.username=:username and o.password=:password",Manager.class)
+               .setParameter("username",username)
+               .setParameter("password",password)
+               .getResultList();
     }
 
     public List<Manager> findByEmail(String email) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("email", email+"%");
-            return repository.executeQuery("Manager.FindByEmail", params, Manager.class);
-        }
+        return entityManager
+        .createQuery("select o from managerEntity o where o.email like :email",Manager.class)
+                .setParameter("email",email)
+                .getResultList();
     }
 
     public Manager findByPhoneNumber(String phoneNumber) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("phoneNumber", phoneNumber);
-            List<Manager> result = repository.executeQuery("Manager.FindByPhoneNumber", params, Manager.class);
-            if (result.isEmpty()) {
-                return null;
-            } else {
-                return result.get(0);
-            }
+        return entityManager
+                .createQuery("select o from managerEntity o where o.phoneNumber =:phoneNumber", Manager.class)
+                .setParameter("phoneNumber", phoneNumber)
+                .getSingleResult();
+    }
+
+        public Manager fideByNationalCode(String nationalCode) throws Exception {
+            return entityManager
+                    .createQuery("select o from managerEntity o where o.nationalCode=:nationalCode", Manager.class)
+                    .setParameter("nationalCode", nationalCode)
+                    .getSingleResult();
         }
-    }  public Manager fideByNationalCode(String nationalCode) throws Exception {
-        try (CrudRepository<Manager, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("nationalCode", nationalCode);
-            List<Manager> result = repository.executeQuery("FideByNationalCode", params, Manager.class);
-            if (result.isEmpty()) {
-                return null;
-            } else {
-                return result.get(0);
-            }
-        }
-}}
+}
