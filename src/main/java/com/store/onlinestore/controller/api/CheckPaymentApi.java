b8bfc4f203391dfc9a833917a189228d8fa825ce
Mahtab Saedi <mhtb.sd82@gmail.com>
2024-07-12 09:56:15 +0330

    Payment commit

--- src/main/java/com/store/onlinestore/controller/api/CheckPaymentApi.java
+++ src/main/java/com/store/onlinestore/controller/api/CheckPaymentApi.java
@@ -1,11 +1,10 @@
 package com.store.onlinestore.controller.api;
 
-import com.store.onlinestore.model.service.CheckPaymentService;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.GET;
 
 @Path("/check")
 
@@ -13,6 +12,6 @@ public class CheckPaymentApi {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() throws Exception {
-        return Response.ok().entity(CheckPaymentService.getService().findAll()).build();
+        return null;
     }
 }
