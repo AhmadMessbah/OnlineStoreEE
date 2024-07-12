54686950d0eb6aa9ed9caafab131c1dc4f194567
Kianarft <kianarafatmanesh123@gmail.com>
2024-07-12 10:20:20 +0330

    COMMIT

--- src/main/java/com/store/onlinestore/model/service/CashPaymentService.java
+++ src/main/java/com/store/onlinestore/model/service/CashPaymentService.java
@@ -1,63 +1,53 @@
 package com.store.onlinestore.model.service;
 
 import com.store.onlinestore.model.entity.CashPayment;
-import com.store.onlinestore.model.repository.CrudRepository;
-import lombok.Getter;
-
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CashPaymentService {
 
-    @Getter
-    private static CashPaymentService service = new CashPaymentService();
-
-    private CashPaymentService(){
-    }
+    @PersistenceContext(unitName = "store")
+    private EntityManager entityManager;
 
     public CashPayment save(CashPayment cashPayment) throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            return repository.save(cashPayment);
-        }
+        entityManager.persist(cashPayment);
+        entityManager.find(CashPayment.class, 1);
+        return cashPayment;
     }
 
     public CashPayment edit(CashPayment cashPayment) throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            return repository.edit(cashPayment);
+        CashPayment foundCashPayment = entityManager.find(CashPayment.class, cashPayment.getId());
+        if (foundCashPayment != null) {
+            entityManager.merge(cashPayment);
         }
+        return cashPayment;
     }
 
     public CashPayment remove(Long id) throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            return repository.remove(id, CashPayment.class);
+        CashPayment cashPayment = entityManager.find(CashPayment.class, id);
+        if (cashPayment != null) {
+            cashPayment.setDeleted(true);
+            entityManager.merge(cashPayment);
         }
+        return cashPayment;
     }
 
     public List<CashPayment> findAll() throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            return repository.findAll(CashPayment.class);
-        }
+        return entityManager
+                .createQuery("select c from cashEntity c where c.deleted = false", CashPayment.class)
+                .getResultList();
     }
 
     public CashPayment findById(Long id) throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            return repository.findById(id, CashPayment.class);
-        }
+        CashPayment cashPayment = entityManager.find(CashPayment.class, id);
+        return cashPayment;
     }
 
-    public CashPayment FindByDateTime(LocalDateTime dateTime) throws Exception {
-        try (CrudRepository<CashPayment, Long> repository = new CrudRepository<>()) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("DateTime", dateTime);
-            List<CashPayment> result = repository.executeQuery( "Cash.FindByDateTime", params, CashPayment.class);
-            if (result.isEmpty()) {
-                return null;
-            } else {
-                return result.get(0);
-            }
-        }
+    public CashPayment findByDateTime(LocalDateTime localDateTime) throws Exception {
+        CashPayment cashPayment = entityManager.find(CashPayment.class, localDateTime);
+        return cashPayment;
     }
 }
 
