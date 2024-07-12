7ea93cb72789dcfcf2864416e82871261f011bb1
Messbah.A <Messbah.A@Gmail.com>
2024-07-12 13:30:02 +0330

    base, attachment , users (admin,customer,manager,suplier), debuged ... CDI

--- src/main/java/com/store/onlinestore/controller/servlet/InitServlet.java
+++ src/main/java/com/store/onlinestore/controller/servlet/InitServlet.java
@@ -2,6 +2,7 @@ package com.store.onlinestore.controller.servlet;
 
 import com.store.onlinestore.model.entity.Role;
 import com.store.onlinestore.model.entity.User;
+import com.store.onlinestore.model.service.UserService;
 import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,8 +16,13 @@ public class InitServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        User user = User.builder().username("admin").password("admin").deleted(false).build();
-        Role role = Role.builder().role("admin").build();
-        userService.save(user);
+        try{
+            User user = User.builder().username("admin").password("admin").deleted(false).build();
+            Role role = Role.builder().role("admin").build();
+            userService.save(user);
+            System.out.println("Admin Created !!!");
+        }catch (Exception e){
+            System.out.println("Error in init !!!");
+        }
     }
 }
