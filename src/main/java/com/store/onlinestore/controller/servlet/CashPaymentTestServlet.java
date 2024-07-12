ee8be271386cd2db8ff2b08618000f96a8812411
Kianarft <kianarafatmanesh123@gmail.com>
2024-07-12 10:13:30 +0330

    CardPaymentTestServlet.java CashPaymentTestServlet.java

--- src/main/java/com/store/onlinestore/controller/servlet/CashPaymentTestServlet.java
+++ src/main/java/com/store/onlinestore/controller/servlet/CashPaymentTestServlet.java
@@ -1,19 +1,31 @@
 package com.store.onlinestore.controller.servlet;
 
 import com.store.onlinestore.controller.validation.BeanValidator;
+import jakarta.servlet.annotation.HttpMethodConstraint;
 import com.store.onlinestore.model.entity.CashPayment;
-import com.store.onlinestore.model.service.CardPaymentService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.HttpConstraint;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import java.time.LocalDateTime;
+import java.io.IOException;
+
+@ServletSecurity(
+    value = @HttpConstraint(
+        rolesAllowed = {"cashPayment"}
+    ),
+
+    httpMethodConstraints =
+    @HttpMethodConstraint(
+        value = "GET",
+        rolesAllowed = {"cashPayment"}
+    )
+)
 
 @WebServlet("/Cash.test")
-
 public class CashPaymentTestServlet extends HttpServlet {
 
     @Override
@@ -31,22 +43,15 @@ public class CashPaymentTestServlet extends HttpServlet {
                             .paymentDateTime(LocalDateTime.now())
                             .build();
 
-            BeanValidator<CashPayment> cashValidator = new BeanValidator<>();
-
-            if (cashValidator.validate(cashPayment1).isEmpty()) {
-                System.out.println(cashValidator.validate(cashPayment1));
+            // todo : Error cash payment service
+            BeanValidator<CashPayment> cashPaymentBeanValidator = new BeanValidator<>();
+            if (cashPaymentBeanValidator.validate(cashPayment1).isEmpty()) {
+//                System.out.println(CashPaymentService.getService.save(cashPayment1));
             } else {
-                System.out.println(cashValidator.validate(cashPayment1));
+                System.out.println(cashPaymentBeanValidator.validate(cashPayment1));
             }
 
-            if (cashValidator.validate(cashPayment2).isEmpty()) {
-                System.out.println(cashValidator.validate(cashPayment2));
-            } else {
-                System.out.println(cashValidator.validate(cashPayment2));
-            }
-
-            System.out.println(CardPaymentService.getService().findAll());
-
+//            System.out.println(CashPaymentService.getService().save(cashPayment2));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
